8a5251fc3d34a18d2dc87e3ad1d6dd244f3dcf6a
stanley <stanleyc.nwachukwu@gmail.com>
2024-05-31 01:15:40 +0100

    created a seperate provider.tf file

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
 
-
-# Provider
-provider "aws" {
-  region  = "us-east-1"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_instance" "my_ec2" {
   ami           = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
