7d75d036a6e6df7327573e9603a32c1eeca27c55
stanley <stanleyc.nwachukwu@gmail.com>
2024-05-31 02:32:47 +0100

    updated resource in main.tf file

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "git_ec2" {
   ami           = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ec2_for_github-actions"
+    Name = "ec2-for-github-actions"
 
   }
 
